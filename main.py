964a2a2c4be8cb2e767e3fc4bd229e4f1428c637
jibuolly <jibuolly@gmail.com>
2025-05-17 19:56:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,6 @@ async def submit(request: Request):
 @app.get("/")
 async def root():
     return {"message": "API is running!"}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
