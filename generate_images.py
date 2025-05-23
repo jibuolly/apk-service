d8eebe093ba5f16b35a93a24bd6dbef7825e6e33
jibuolly <jibuolly@gmail.com>
2025-05-23 11:19:21 +0530

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -2,6 +2,10 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 import sys
 
+def get_text_size(draw, text, font):
+    bbox = draw.textbbox((0, 0), text, font=font)
+    return bbox[2] - bbox[0], bbox[3] - bbox[1]
+
 def generate_icon(site_name, color_hex):
     size = (512, 512)
     image = Image.new("RGB", size, color_hex)
@@ -14,7 +18,7 @@ def generate_icon(site_name, color_hex):
         font = ImageFont.load_default()
 
     text = site_name[0].upper()
-    text_width, text_height = draw.textsize(text, font=font)
+    text_width, text_height = get_text_size(draw, text, font)
     position = ((size[0] - text_width) / 2, (size[1] - text_height) / 2)
     draw.text(position, text, fill="white", font=font)
     return image
@@ -31,7 +35,7 @@ def generate_splash(site_name, color_hex):
         font = ImageFont.load_default()
 
     text = site_name.capitalize()
-    text_width, text_height = draw.textsize(text, font=font)
+    text_width, text_height = get_text_size(draw, text, font)
     position = ((size[0] - text_width) / 2, (size[1] - text_height) / 2)
     draw.text(position, text, fill="white", font=font)
     return image
